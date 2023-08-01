5430f2406a9e0da863393d0fbfbd92138cb8ab10
katarighe <m.ighe@hotmail.com>
2023-08-01 16:51:16 +0300

    Not needed

--- src/modules/getmeal.js
+++
@@ -1,8 +0,0 @@
-const getMeal =  async(id)=> {
-
-    const response = await fetch(`http://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`);
-    const data = await response.json();
-    return data.meals[0];
-  }
-  
-  module.exports = getMeal

f1f7b3ea379b99e8a245ae61cb5b6e2f6ad19d4e
katarighe <m.ighe@hotmail.com>
2023-08-01 17:54:03 +0300

    Fix errors relating to the API

--- src/modules/display-meals.js
+++ src/modules/display-meals.js
@@ -3,7 +3,7 @@ import count from "./meal-count.js";
 import errorMessage from "./error-message.js";
 import fetchLikes from "./display-likes.js";
 import likeMeal from "./likes.js";
-// import heart from '../';
+import heart from '../';
 
 const displayMeals = document.querySelector('.display-meals');
 
@@ -12,8 +12,8 @@ count();
 const displayList = async () => {
   try {
     const response = await axios.get("http://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood");
-    const result = response.data;
-    const meals = result.splice(0, 15);
+    const data = response.data;
+    const meals = data.meals;
 
     meals.forEach(async (meal) => {
       const img = document.createElement("img");
@@ -22,8 +22,8 @@ const displayList = async () => {
       const card = document.createElement("div");
       card.className = "card";
       card.innerHTML = `
-          <img src="${meal.image.medium}" alt="${meal.name}" class="image">
-          <h3>${meal.name}</h3>
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="image">
+          <h3>${meal.strMeal}</h3>
           <div class="card-body">
             <button class="commentBtn">Comment</button>
             <p>${img.outerHTML}<span class="like-count">0</span> Likes</p>
@@ -31,22 +31,22 @@ const displayList = async () => {
         `;
       displayMeals.appendChild(card);
 
-      const likeCount = card.querySelector(".like-count");
-      const likes = await fetchLikes(meal.id);
+      const likeCount = card.querySelector('.like-count');
+      const likes = await fetchLikes(meal.idMeal);
       likeCount.textContent = `${likes}`;
 
-      const likeIcon = card.querySelector(".like-icon");
+      const likeIcon = card.querySelector('.like-icon');
       likeIcon.addEventListener("click", async () => {
-        await likeMeal(meal.id);
-        const likes = await fetchLikes(meal.id);
+        await likeMeal(meal.idMeal);
+        const likes = await fetchLikes(meal.idMeal);
         likeCount.textContent = `${likes}`;
       });
 
-      const commentBtn = card.querySelector("commentBtn");
-      const popContainer = document.querySelector("popContainer");
+      const commentBtn = card.querySelector('commentBtn');
+      const popContainer = document.querySelector('popContainer');
       commentBtn.addEventListener("click", async (e) => {
         e.preventDefault();
-        const mealid = meal.id;
+        const mealid = meal.idMeal;
         await displayCommentPop(mealid);
         document.body.style.overflow = "hidden";
         popContainer.style.display = "block";
--- src/modules/meal-count.js
+++ src/modules/meal-count.js
@@ -5,8 +5,8 @@ const mealCount = document.querySelector('.meal-count');
 const count = async () => {
     try {
         const response = await axios.get('http://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
-        const result = response.data;
-        const meals = result.splice(0, 15);
+        const data = response.data;
+        const meals = data.meals;
         mealCount.innerHTML = meals.length.toString();
     } catch (error) {
         console.error('Error!:', error);

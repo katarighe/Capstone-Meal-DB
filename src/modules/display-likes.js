eccc3063c8797cde85da4199168144ac6d4e47ad
katarighe <m.ighe@hotmail.com>
2023-08-02 15:36:24 +0300

    Make minor fixes and punctionality errors

--- src/modules/display-likes.js
+++ src/modules/display-likes.js
@@ -4,14 +4,14 @@ import errorMsg from './error-message.js';
 
 const fetchLikes = async (idMeal) => {
   try {
-    const response = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppName()}/likes/`);
+  const response = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppName()}/likes/`);
   const likes = response.data;
   const itemLikes = likes.find((like) => like.item_id === idMeal);
-  return itemLikes ? itemLikes.likes : 0
+  return itemLikes ? itemLikes.likes : 0;
 } catch (error) {
   errorMsg('Error!', 'red');
   return 0;
- }
+  }
 };
 
 export default fetchLikes;

80efe531af2844684e5ccc5377624c3ed2a6553e
katarighe <m.ighe@hotmail.com>
2023-08-02 15:26:14 +0300

    Update meals count

--- src/modules/mealsCount.js
+++ src/modules/mealsCount.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const mealsCount = document.querySelector('.meal-count');
+const mealsCount = document.querySelector('.meals-count');
 
 const count = async () => {
   try {

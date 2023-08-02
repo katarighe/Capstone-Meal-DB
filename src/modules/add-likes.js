b3b014dd251cd00f8e6b39be9f4c78da0deaf62a
katarighe <m.ighe@hotmail.com>
2023-08-02 15:24:11 +0300

    Fix typo error

--- src/modules/add-likes.js
+++ src/modules/add-likes.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import getAppName from './local-storage.js';
 import errorMessage from './error-message.js';
 
-const likeMeal = async (mealId) => {
+const likeMeal = async (idMeal) => {
   try {
     const response = await axios.post(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppName()}/likes`, {
-      item_id: mealId,
+      item_id: idMeal,
     });
     errorMessage(`Meal like ${response.data}`, 'green');
   } catch (error) {

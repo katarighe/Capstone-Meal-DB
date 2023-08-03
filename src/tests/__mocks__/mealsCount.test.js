d014f0ef7109936d1124b0174d4841442eaa539c
katarighe <m.ighe@hotmail.com>
2023-08-03 11:48:24 +0300

    Fix file path

--- src/tests/__mocks__/mealsCount.test.js
+++ src/tests/__mocks__/mealsCount.test.js
@@ -1,5 +1,5 @@
-import axios from './axios';
-import count from './mealsCount.js';
+import axios from './__mocks__/axios.js';
+import count from '../src/modules/mealsCount.js';
 
 describe('Count total meals available', () => {
     let mealsCountElement;

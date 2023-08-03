92ce19379460e1473ce39024eafd996e98f9a033
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-08-03 16:04:56 +0530

    Add test for comment

--- src/modules/countComment.js
+++ src/modules/countComment.js
@@ -1,10 +1,10 @@
 const commentCounter = async () => {
-    const commentsContainer = document.querySelector('.commentDiv');
-    const commentCount = commentsContainer.children.length;
-    const counterElement = document.querySelector('.counter');
-    if (counterElement) {
-        counterElement.textContent = `(${commentCount})`;
-    }
+  const commentsContainer = document.querySelector('.commentDiv');
+  const commentCount = commentsContainer.children.length;
+  const counterElement = document.querySelector('.counter');
+  if (counterElement) {
+    counterElement.textContent = `(${commentCount})`;
+  }
 };
 
 export default commentCounter;

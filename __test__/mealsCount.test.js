be365b0bda300b4de56e586d34f31589c1929fa2
katarighe <m.ighe@hotmail.com>
2023-08-03 13:06:30 +0300

    Rearrange the test and mock files

--- __test__/mealsCount.test.js
+++ __test__/mealsCount.test.js
@@ -1,5 +1,5 @@
-import axios from './axios.js';
-import count from '../src/modules/mealsCount.js';
+import axios from '../__mocks__/axios.js';
+import count from '../src/modules/countComment.js';
 
 describe('Count total meals available', () => {
   let mealsCountElement;

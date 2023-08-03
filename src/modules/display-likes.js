59d86540481de9586dac6e203ab1f97b0b09a28b
katarighe <m.ighe@hotmail.com>
2023-08-03 12:07:01 +0300

    Fix spacing iden

--- src/modules/display-likes.js
+++ src/modules/display-likes.js
@@ -4,13 +4,13 @@ import errorMessage from './error-message.js';
 
 const fetchLikes = async (idMeal) => {
   try {
-  const response = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppName()}/likes/`);
-  const likes = response.data;
-  const itemLikes = likes.find((like) => like.item_id === idMeal);
-  return itemLikes ? itemLikes.likes : 0;
-} catch (error) {
-  errorMessage('Error!', 'red');
-  return 0;
+    const response = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppName()}/likes/`);
+    const likes = response.data;
+    const itemLikes = likes.find((like) => like.item_id === idMeal);
+    return itemLikes ? itemLikes.likes : 0;
+  } catch (error) {
+    errorMessage('Error!', 'red');
+    return 0;
   }
 };
 
